39294a072877eba793e4ed8293fa0fd102a70d31
Chris Forrette <chris@chrisforrette.com>
2010-09-03 12:27:49 -0700

    Switching to a tab navigation app

---
+++ iphone/Barbarossa/Classes/BarbarossaAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BarbarossaAppDelegate.h
+//  Barbarossa
+//
+//  Created by Chris Forrette on 9/3/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarbarossaAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
