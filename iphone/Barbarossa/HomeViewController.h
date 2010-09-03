201cbbab589c52e9692b779b35c949be61dd9a19
Chris Forrette <chris@chrisforrette.com>
2010-09-03 13:02:48 -0700

    Skeleton controllers and nib for home, locations, and near me views

---
+++ iphone/Barbarossa/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Barbarossa
+//
+//  Created by Chris Forrette on 9/3/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HomeViewController : UIViewController {
+
+}
+
+@end
